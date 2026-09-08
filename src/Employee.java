dbaaa321d21914748305e787372910c04461b902
Supriya Gundepalli <supriyagundepalli@gmail.com>
2026-09-08 14:46:29 +0530

    Add Employee class with attributes and methods

---
+++ src/Employee.java
@@ -0,0 +1,36 @@
+public class Employee {
+    private int id;
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void displayEmployee() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: " + salary);
+    }
+}
